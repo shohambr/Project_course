bf3d220ca7efbde061742468250481faf4d8aec2
Hadar Elkayam <hadarel4@gmail.com>
2025-04-15 16:53:07 +0300

    fixing payment

--- src/main/java/DomainLayer/PaymentSystem.java
+++
@@ -1,5 +0,0 @@
-package DomainLayer;
-
-public interface PaymentSystem {
-    void processPayment(double payment, String creditCardNumber, String expirationDate, String backNumber) throws Exception;
-}

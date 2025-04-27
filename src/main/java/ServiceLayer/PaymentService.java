b7c7e931bdf1d5a12c82ae4c99efe8770a3940c4
Hadar Elkayam <hadarelk@post.bgu.ac.il>
2025-04-27 07:08:18 +0300

    added event and error logger

--- src/main/java/ServiceLayer/PaymentService.java
+++ src/main/java/ServiceLayer/PaymentService.java
@@ -1,6 +1,7 @@
 package ServiceLayer;
 
 import DomainLayer.IPayment;
+import DomainLayer.User;
 
 public class PaymentService {
 
@@ -10,13 +11,15 @@ public class PaymentService {
         this.paymentSystem = paymentSystem;
     }
 
-    public boolean processPayment(String stringPayment, String creditCardNumber, String expirationDate, String backNumber) {
+    public boolean processPayment(User user, String stringPayment, String creditCardNumber, String expirationDate, String backNumber) {
         try {
             Double payment = new Double(stringPayment);
             paymentSystem.processPayment(payment, creditCardNumber, expirationDate, backNumber);
+            EventLogger.logEvent(user.getID(), "Successfully payed: " + stringPayment);
         return true;
         } catch (Exception e) {
             System.out.println("Error encountered while processing payment:" + e.getMessage());
+            ErrorLogger.logError(user.getID(), "Failed to pay"     , e.getMessage());
             return false;
         }
     }

482e1513c4ed3aec9a044020d8b753e8fc9baea2
Hadar Elkayam <hadarelk@post.bgu.ac.il>
2025-04-27 07:14:15 +0300

    added event and error logger

--- src/main/java/ServiceLayer/ShippingService.java
+++ src/main/java/ServiceLayer/ShippingService.java
@@ -1,6 +1,7 @@
 package ServiceLayer;
 import DomainLayer.IPayment;
 import DomainLayer.IShipping;
+import DomainLayer.User;
 
 import java.util.*;
 
@@ -11,12 +12,14 @@ public class ShippingService {
         this.shippingSystem = shippingSystem;
     }
 
-    public boolean processShipping(String state, String city, String street, String homeNumber) {
+    public boolean processShipping(User user, String state, String city, String street, String homeNumber) {
         try {
             shippingSystem.processShipping(state, city, street, homeNumber);
+            EventLogger.logEvent(user.getID(), "Shipping successful");
             return true;
         } catch (Exception e) {
             System.out.println("Error encountered while processing shipping:" + e.getMessage());
+            ErrorLogger.logError(user.getID(), "Error in shipping", e.getMessage());
             return false;
         }
     }
--- src/test/java/ServiceLayer/ShippingServiceTest.java
+++ src/test/java/ServiceLayer/ShippingServiceTest.java
@@ -1,5 +1,7 @@
 package ServiceLayer;
 
+import DomainLayer.Roles.Guest;
+import DomainLayer.User;
 import infrastructureLayer.ProxyShipping;
 import Mocks.MockShipping;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,47 +12,49 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShippingServiceTest {
 
     private ShippingService shippingService;
+    private User user;
 
     @BeforeEach
     void setUp() {
         MockShipping mockShipping = new MockShipping();
         ProxyShipping proxyShipping = new ProxyShipping(mockShipping);
         shippingService = new ShippingService(proxyShipping);
+        User user = new Guest();
     }
 
     @Test
     public void testProcessShipping_Successful() {
-        boolean response = shippingService.processShipping("Israel", "Be'er Sheva", "Even Gvirol", "12");
+        boolean response = shippingService.processShipping(user, "Israel", "Be'er Sheva", "Even Gvirol", "12");
         assertTrue(response);
     }
 
     @Test
     public void testProcessShipping_EmptyState_Failure() {
-        boolean response = shippingService.processShipping("", "Be'er Sheva", "Even Gvirol", "12");
+        boolean response = shippingService.processShipping(user, "", "Be'er Sheva", "Even Gvirol", "12");
         assertFalse(response);
     }
 
     @Test
     public void testProcessShipping_EmptyCity_Failure() {
-        boolean response = shippingService.processShipping("Israel", "", "Even Gvirol", "12");
+        boolean response = shippingService.processShipping(user, "Israel", "", "Even Gvirol", "12");
         assertFalse(response);
     }
 
     @Test
     public void testProcessShipping_EmptyStreet_Failure() {
-        boolean response = shippingService.processShipping("Israel", "Be'er Sheva", "", "12");
+        boolean response = shippingService.processShipping(user, "Israel", "Be'er Sheva", "", "12");
         assertFalse(response);
     }
 
     @Test
     public void testProcessShipping_InvalidHomeNumber_Failure() {
-        boolean response = shippingService.processShipping("Israel", "Be'er Sheva", "Even Gvirol", "vjmikod");
+        boolean response = shippingService.processShipping(user, "Israel", "Be'er Sheva", "Even Gvirol", "vjmikod");
         assertFalse(response);
     }
 
     @Test
     public void testProcessPayment_EmptyHomeNumber_Failure() {
-        boolean response = shippingService.processShipping("Israel", "Be'er Sheva", "Even Gvirol", "");
+        boolean response = shippingService.processShipping(user, "Israel", "Be'er Sheva", "Even Gvirol", "");
         assertFalse(response);
     }
 
